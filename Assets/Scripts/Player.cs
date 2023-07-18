dc6afa332af1d6adc8969d8edc587dc40cfe58f2
Josh Guinto <134356678+Marcutech0@users.noreply.github.com>
2023-07-18 12:40:51 +0800

    bullets fixed
    bullets fixed and background addded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,82 +5,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Player speed
+    // Player speed
     public float moveSpeed;
 
-    //Rigid Body access
+    // Rigid Body access
     public Rigidbody2D rigidBody;
 
-    //Player movement
+    // Player movement
     private Vector2 movementInput;
-
-
-
-    //coin collecting
-    public int CoinCount;
-
-    //player healt
-    public int HealthPoints;
-
-    //for Shells
+    // For Shells
     public GameObject ShellPrefab;
-    public Transform ShellSpawnPoints;
-    public Transform ShellSpawnPoints2;
+    public Transform ShellSpawnPoint;
+    public Transform ShellSpawnPoint2;
     public float ShellSpeed;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        //para sa player to pre kimi lang
+        // Get the reference to the Rigidbody2D component
         rigidBody = GetComponent<Rigidbody2D>();
-
     }
 
     // PLAY/LAUNCH
     void Update()
     {
-       
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject Shell = Instantiate(ShellPrefab, ShellSpawnPoints.position,Quaternion.identity);
-            GameObject Shell2 = Instantiate(ShellPrefab, ShellSpawnPoints2.position, Quaternion.identity);
+            GameObject Shell = Instantiate(ShellPrefab, ShellSpawnPoint.position, Quaternion.identity);
+            GameObject Shell2 = Instantiate(ShellPrefab, ShellSpawnPoint2.position, Quaternion.identity);
             Rigidbody2D rb = Shell.GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.up * ShellSpeed; // Shoots the shell only in the upward direction
             Rigidbody2D rb2 = Shell2.GetComponent<Rigidbody2D>();
-            rb.velocity = transform.up * ShellSpeed;
-            rb2.velocity = transform.up * ShellSpeed;
+            rb2.velocity = Vector2.up * ShellSpeed; // Shoots the shell only in the upward direction
         }
     }
-    //Fixed for physx kimi lang pre
+
+    // Fixed for physics update
     private void FixedUpdate()
     {
-        //player  movement
+        // Player movement
         rigidBody.velocity = movementInput * moveSpeed;
     }
 
-    //input keybinds
+    // Input keybinds
     private void OnMove(InputValue inputValue)
     {
-        // When A is pressed
+        // Get the movement input
         movementInput = inputValue.Get<Vector2>();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Speed"))
         {
-            Transform col = collision.transform;
-            col.transform.position = new Vector2(999, 999);
-
-        }
-        if (collision.CompareTag("COIN_PREFAB"))
-        {
-            CoinCount++;
-            Destroy(collision.gameObject);
-        }
-        if (collision.CompareTag("Health"))
-        {
-            Destroy(collision.gameObject);
+            // Move the collided object off-screen
+            collision.transform.position = new Vector2(999, 999);
         }
     }
 }
